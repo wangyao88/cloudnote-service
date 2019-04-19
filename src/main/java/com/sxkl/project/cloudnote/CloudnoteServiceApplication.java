e9b678a33d02edf1f925d8d6375e79d64b94f0ac
wangyao <wangyao@cis.com.cn>
2019-04-19 15:04:42 +0800

    将springboot项目打成war包

--- src/main/java/com/sxkl/project/cloudnote/CloudnoteServiceApplication.java
+++ src/main/java/com/sxkl/project/cloudnote/CloudnoteServiceApplication.java
@@ -3,14 +3,21 @@ package com.sxkl.project.cloudnote;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Slf4j
 @SpringBootApplication
-public class CloudnoteServiceApplication {
+public class CloudnoteServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudnoteServiceApplication.class, args);
 		log.info("***************启动成功！**************");
 		log.info("**********You Know, for Search*******");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CloudnoteServiceApplication.class);
 	}
 }
